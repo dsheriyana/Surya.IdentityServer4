adb88180001702e1f9ea69315b8500854e8ee0aa
dsury <dsury@DESKTOP-RJGNV5T>
2022-02-02 05:48:28 +0700

    Add project files.

---
+++ Surya.IdentityServer4/IdentityConfiguration/Users.cs
@@ -0,0 +1,32 @@
+﻿using IdentityModel;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Surya.IdentityServer4.IdentityConfiguration
+{
+    public class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser>
+        {
+            new TestUser
+            {
+                SubjectId = "56892347",
+                Username = "dsh",
+                Password = "123",
+                Claims = new List<Claim>
+                {
+                    new Claim(JwtClaimTypes.Email, "dsuryaheriyana@gmail.com"),
+                    new Claim(JwtClaimTypes.Role, "admin"),
+                    new Claim(JwtClaimTypes.WebSite, "https://dsh.my.id")
+                }
+            }
+        };
+        }
+    }
+}
